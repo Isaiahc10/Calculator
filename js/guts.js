c76391142c76229c8f3c46a4bf2040cea138b29d
Isaiahc10 <icanty10@gmail.com>
2015-05-26 21:00:45 +0000

    first commit

---
+++ js/guts.js
@@ -0,0 +1,32 @@
+//divide(x, y);
+//multiply(x, y);
+//add(x, y);
+//subtract(x, y);
+//power(x, y);
+//square_root(x)
+//logBase10(x)
+//generateRandomNumber();
+function divide(x,y){
+  return x/y;
+}
+function multiply(x,y){
+  return x*y;
+}
+function add(x,y){
+  return x+y;
+}
+function subtract(x,y){
+  return x-y;
+}
+function power(x,y){
+  return Math.pow(x,y);
+}
+function square_root(x){
+  return Math.sqrt(x);
+}
+function logBase10(x){
+  return Math.log(x)/Math.log(10);
+}
+function generateRandomNumber(){
+  return 
+}
